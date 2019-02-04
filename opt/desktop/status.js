a7d64f459b5a53eac96596172e2a0b1b3844407c
Sam Lanning <sam@samlanning.com>
2019-02-03 18:14:13 -0800

    Add battery info to status

--- opt/desktop/status.js
+++ opt/desktop/status.js
@@ -1,8 +1,33 @@
 const fs = require('fs');
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
 
-setInterval(() => {
+const ACPI_REGEX = /\s([a-zA-Z]+)\, ([0-9]+)\%\, ([^\s]+)\s/;
+const COLOR_GREEN = '#86df32'
+const COLOR_RED = '#d25252'
+const COLOR_BLUE = '#1e90ff'
+
+setInterval(async () => {
 
     const status = [];
+
+    const batt = (await exec('acpi -b')).stdout;
+    const battRegex = ACPI_REGEX.exec(batt);
+    if (battRegex) {
+        const [_, battStatus, battPercent, battRemaining] = battRegex
+        if (battStatus === 'Charging') {
+            status.push({
+                full_text: `CHARGING: ${battPercent}% (${battRemaining} until full)`,
+                color: COLOR_GREEN
+            });
+        } else {
+            const percent = parseInt(battPercent)
+            status.push({
+                full_text: `${battPercent}% (${battRemaining} remaining)`,
+                color: percent < 20 ? COLOR_RED : COLOR_BLUE
+            });
+        }
+    }
 
     const date = new Date();
     status.push({
